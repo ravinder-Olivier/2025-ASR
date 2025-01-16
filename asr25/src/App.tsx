e00afffde04e2a87924e2340d33ea52df23e5c30
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2025-01-16 08:05:26 -0800

    udpatesr
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- asr25/src/App.tsx
+++ asr25/src/App.tsx
@@ -8,26 +8,12 @@ function App() {
   const [name, setName] = createSignal("");
 
   async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name: name() }));
   }
 
   return (
     <main class="container">
-      <h1>Welcome to Tauri + Solid</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
+      <h1>ASR25</h1>
 
       <form
         class="row"
@@ -39,7 +25,7 @@ function App() {
         <input
           id="greet-input"
           onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          placeholder="Enter a prompt..."
         />
         <button type="submit">Greet</button>
       </form>
