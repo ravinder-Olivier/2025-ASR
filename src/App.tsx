052459be581d98b56141024c4ca34ad4764c2cf7
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2025-01-17 11:56:29 -0800

    Builds succesfully
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 Ravinder Olivier Singh Dadiala
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import { createSignal } from "solid-js";
 import "./App.css";
 import {promptHandler} from "./lib/promptHandler.ts";
@@ -9,7 +25,6 @@ function App() {
   const [name, setName] = createSignal("");
 
   async function prompt() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setPromptMsg (await promptHandler(name()));
   }
 
@@ -27,9 +42,9 @@ function App() {
         <input
           id="prompt-input"
           onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          placeholder="Enter a prompt..."
         />
-        <button type="submit">Greet</button>
+        <button type="submit">Create</button>
       </form>
       <p>{promptMsg()}</p>
     </main>
